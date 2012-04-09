12301e88156683ad42a74e34b3a3d38c9284491b
Michael Hausenblas <michael.hausenblas@gmail.com>
2012-04-09 15:12:42 +0100

    added basic profile vocab, some more documentation and enhanced basic serialisation of resource

--- README.md
+++ README.md
@@ -2,8 +2,30 @@
 
 An object-orient implementation of '[Linked Data Basic Profile](http://www.w3.org/Submission/2012/SUBM-ldbp-20120326/ "Linked Data Basic Profile 1.0")'-compliant containers/resources.
 
+## Usage
+
+After you got the source somewhere local (via git clone or download facility) you want to compile it:
+
+	scalac -cp lib/scardf-0.6-SNAPSHOT.jar:lib/joda-time-1.6.jar info.paygoo.core.scala
+
+Then you can run the built-in test:
+
+	scala -cp lib/scardf-0.6-SNAPSHOT.jar:lib/joda-time-1.6.jar info.paygoo.core.PayGooResource
+
+...which should yield something like the following:
+
+	<div>About <a href='http://data.example.com/test#it'>pg0</a>, last updated 2012-04-09</div>
+	{"base" : "http:\/\/data.example.com\/test#", "label" : "pg0", "date" : 2012-04-09}
+	base=http://data.example.com/test#, label=pg0, date=2012-04-09
+	<http://data.example.com/test#it> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://schema.org/Thing> .
+	<http://data.example.com/test#it> <http://purl.org/dc/terms/title> "pg0" .
+	<http://data.example.com/test#it> <http://purl.org/dc/terms/date> "2012-04-09"^^<http://www.w3.org/2001/XMLSchema#date> .
+
+	[PayGooResource: base=http://data.example.com/test# | label=pg0 | date=2012-04-09]
+
 ## Dependencies
 
+* Scala 2.9.1
 * [scardf](http://code.google.com/p/scardf/ "Scala RDF API - Google Project Hosting")
 * [spray](spray.cc "A suite of lightweight Scala libraries for building and consuming RESTful web services on top of Akka")
 
--- info.paygoo.core.scala
+++ info.paygoo.core.scala
@@ -1,27 +1,32 @@
 import scala.actors.Actor
 import scala.actors.Actor._
 import scala.util.parsing.json._
+import org.scardf._
+import org.joda.time.LocalDate
 
 package info.paygoo.core {
 	
 	/** 
-	 * Objects that represent a PayGoo's serialisation (or wire format).
-	 * A PayGoo MUST support all four media types.
+	 * Represent a PayGoo's serialisation (aka wire format).
+	 * A PayGoo MUST support all of the following four media types.
 	 * 
 	 * @param mediatype as of http://www.iana.org/assignments/media-types/
 	 */
-	case class WireFormat ( val mediatype: String )
-	object HTML extends WireFormat ( mediatype = "text/html" )
-	object JSON extends WireFormat ( mediatype = "application/json" )
-	object Text extends WireFormat ( mediatype = "text/plain" )
-	object Turtle extends WireFormat ( mediatype = "text/turtle" )
+	case class WireFormat ( val mediatype: String, val target: String )
+	object HTML extends WireFormat ( mediatype = "text/html", target = "human" )
+	object JSON extends WireFormat ( mediatype = "application/json", target = "machine" )
+	object Text extends WireFormat ( mediatype = "text/plain", target = "human" )
+	object NTriple extends WireFormat ( mediatype = "text/plain", target = "machine" )
 
 	/** 
 	 * The core PayGoo class, just has a label. 
 	 * 
 	 * @param label a human-readable label for the PayGoo
 	 */
-	abstract class PayGoo ( var label: String ) {
+	abstract class PayGoo (val base: String, var label: String ) {
+		val s = Vocabulary( "http://schema.org/" )
+		val dc = Vocabulary( "http://purl.org/dc/terms/" )
+		val bp = Vocabulary( "http://open-services.net/ns/basicProfile#" )
 		def ser( format: WireFormat = JSON ) : String
 	}
 
@@ -31,17 +36,24 @@ package info.paygoo.core {
 	 * @param rlabel a human-readable label for the PayGoo resource
 	 * @return dunno
 	 */
-	case class PayGooResource ( rlabel: String ) extends PayGoo ( rlabel ) {
-		private var r = Map ( "label" -> rlabel )
+	case class PayGooResource (rbase: String, rlabel: String ) extends PayGoo ( rbase, rlabel ) {
+		private var r = Map ( "base" -> rbase, "label" -> rlabel, "date" -> new LocalDate())
+		private val rself = r("base") + "it"
 		
 		override def ser ( format: WireFormat = JSON ) : String = format match {
-			case HTML => "<div>" + rlabel + "</div>"
+			case HTML => "<div>About <a href='" + rself + "'>" + r("label") + "</a>, last updated " + r("date") + "</div>"
 			case JSON => JSONObject(r).toString
-			case Text => r("label")
-			case Turtle => ":a rdfs:label '" + r("label") + "' ."
+			case Text => "base=" + r("base") + ", label=" + r("label") + ", date=" + r("date") 
+			case NTriple => val g = Graph.build(	UriRef( rself ) - (
+													RDF.Type -> s.uriref("Thing"),
+													dc.uriref("title") ->  r("label"),
+													dc.uriref("date") ->  r("date")
+												)
+									)
+									g.rend
 		}
 
-		override def toString = "[PayGooResource: label=" + r("label") + "]"
+		override def toString = "[PayGooResource: base=" + r("base") + " | label=" + r("label") + " | date=" + r("date") + "]"
 	}
 
 	
@@ -49,11 +61,11 @@ package info.paygoo.core {
 	 * Testing the resource ... 
 	 */
 	object PayGooResource extends App {
-		val r = new PayGooResource("pg0")
+		val r = new PayGooResource("http://data.example.com/test#", "pg0")
 		println(r.ser(format=HTML))
 		println(r.ser(format=JSON))
 		println(r.ser(format=Text))
-		println(r.ser(format=Turtle))
+		println(r.ser(format=NTriple))
 		println(r)
 	}
 }
